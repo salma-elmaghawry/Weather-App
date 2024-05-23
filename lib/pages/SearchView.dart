bdf14bfe54ebc132f28a8b1d584b1469c643cb31
salma <salmaelmaghawry91@gmail.com>
2024-05-23 05:08:21 +0300

    onchange &onsubmitted

--- lib/pages/SearchView.dart
+++ lib/pages/SearchView.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class SearchView extends StatelessWidget {
@@ -6,34 +8,34 @@ class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(title: Text('Search for the City'),
-    centerTitle: true,
-    titleTextStyle: const TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-          ),
-          ), 
-          
-          body: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child:  Center(
-              child:  TextField(
-
-                decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 20,vertical: 20),
-                  labelText: 'Search',
-                  suffixIcon: Icon(Icons.search),
-                  //suffixIconColor: Colors.amber,
-                  border:OutlineInputBorder(
-                  borderRadius:BorderRadius.circular(16),  
-                  ) ,
-                  hintText: 'Enter City Name',
-                  )
+      appBar: AppBar(
+        title: Text('Search for the City'),
+        centerTitle: true,
+        titleTextStyle: const TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: TextField(
+              onSubmitted: (value) {
+                log(value);
+              },
+              decoration: InputDecoration(
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                labelText: 'Search',
+                suffixIcon: Icon(Icons.search),
+                //suffixIconColor: Colors.amber,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
                 ),
-            
-              ),
-            ),
-          );
-    
+                hintText: 'Enter City Name',
+              )),
+        ),
+      ),
+    );
   }
 }
