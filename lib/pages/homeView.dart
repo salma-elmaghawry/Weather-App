838a9dc6cb57445d7721a25d056886b7c369da34
salma <salmaelmaghawry91@gmail.com>
2024-06-11 16:26:47 +0300

    create weather service

--- lib/pages/homeView.dart
+++ lib/pages/homeView.dart
@@ -16,6 +16,7 @@ class homeView extends StatelessWidget {
                     MaterialPageRoute(
                       builder: (context) {
                         return SearchView();
+                        
                       },
                     ),
                   );
