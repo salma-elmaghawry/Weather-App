be1a90e758104f0174a4320e4aa5f124e82a6c2a
salma <salmaelmaghawry91@gmail.com>
2024-06-11 17:14:37 +0300

    weathermodel

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,18 +1,31 @@
+import 'package:flutter/foundation.dart';
+
 class WeatherModel {
   final String cityName;
   final String date;
   final String? image;
-  final String? temp;
+  final String temp;
   final String maxTemp;
   final String minTemp;
-  final String? weatherTemp;
+  final String? weatherCondition;
 
   WeatherModel(
       {required this.cityName,
       required this.date,
-      required this.image,
+      this.image,
       required this.temp,
       required this.maxTemp,
       required this.minTemp,
-      required this.weatherTemp});
+      required this.weatherCondition});
+
+  factory WeatherModel.fromJson(json) {
+    return WeatherModel(
+        image: json['forecast']['forecastday'][0]['day']['condition']['icon'],
+        cityName: json['location']['name'],
+        date: json['current']['last_updated'],
+        temp: json['forecast']['forecastday'][0]['day']['avgtemp_c'],
+        maxTemp: json['forecast']['forecastday'][0]['day']['maxtemp_c'],
+        minTemp: json['forecast']['forecastday'][0]['day']['mintemp_c'],
+        weatherCondition: json['forecast']['forecastday'][0]['day']['condition']['text']);
+  }
 }
