6127f89de040801015ab65324bf6d1113d0630a0
salma <salmaelmaghawry91@gmail.com>
2024-06-18 14:55:02 +0300

    baseURL & apikey

--- lib/Services/weather_Service.dart
+++ lib/Services/weather_Service.dart
@@ -3,10 +3,12 @@ import 'package:weather/models/weather_model.dart';
 
 class WeatherService {
   final Dio dio;
+  final String baseURL = 'https://api.weatherapi.com/v1';
+  final String ApiKey = 'b2a3f13c46dc45f5adc84756242805';
   WeatherService(this.dio);
   Future<WeatherModel> getCurrentWeather({required String cityName}) async {
-    Response response = await dio.get(
-        'https://api.weatherapi.com/v1/forecast.json?key=b2a3f13c46dc45f5adc84756242805&q=$cityName&days=1');
+    Response response =
+        await dio.get('$baseURL/forecast.json?key=$ApiKey&q=$cityName&days=1');
     WeatherModel weathermodel = WeatherModel.fromJson(response.data);
     return weathermodel;
   }
