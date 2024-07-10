f103f116469ef1874f88f58bd9141f4d65d8dc91
salma <salmaelmaghawry91@gmail.com>
2024-07-10 04:41:51 +0300

    done v1

--- lib/Widgets/WeatherInfoBody.dart
+++ lib/Widgets/WeatherInfoBody.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather/cubits/get_weather_cubit/get_weather_cubit.dart';
 import 'package:weather/cubits/get_weather_cubit/get_weather_states.dart';
+import 'package:weather/main.dart';
 import 'package:weather/models/weather_model.dart';
 
 class WeatherInfoBody extends StatelessWidget {
@@ -18,53 +19,59 @@ class WeatherInfoBody extends StatelessWidget {
         } else if (state is WeatherLoadedState) {
           WeatherModel weatherModel = state.weatherModel;
 
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  weatherModel.cityName,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 25),
-                ),
-                Text(
-                  'Updated at ${weatherModel.date.hour}:${weatherModel.date.minute}',
-                  style: const TextStyle(fontSize: 24),
-                ),
-                const SizedBox(height: 32),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.network("https:${weatherModel.image}"),
-                    Text(
-                      weatherModel.temp.toString(),
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          'Maxtemp: ${weatherModel.maxTemp.round()}',
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 15),
-                        ),
-                        Text(
-                          'Mintemp: ${weatherModel.minTemp.round()}',
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 15),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 40),
-                Text(
-                  weatherModel.weatherCondition.toString(),
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 25),
-                ),
-              ],
+          return Container(
+            decoration: const BoxDecoration(gradient:LinearGradient(
+              begin: Alignment.topCenter,end: Alignment.bottomCenter,
+              colors: [ Color(0xff62a1c7),Color(0xffFFBF78)],
+              )),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    weatherModel.cityName,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 35),
+                  ),
+                  Text(
+                    'Updated at ${weatherModel.date.hour.toString()}:${weatherModel.date.minute.toString()}',
+                    style: const TextStyle(fontSize: 15),
+                  ),
+                  const SizedBox(height: 32),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.network("https:${weatherModel.image}"),
+                      Text(
+                        weatherModel.temp.toString(),
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            'Maxtemp: ${weatherModel.maxTemp.round()}',
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 15),
+                          ),
+                          Text(
+                            'Mintemp: ${weatherModel.minTemp.round()}',
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 15),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 40),
+                  Text(
+                    weatherModel.weatherCondition.toString(),
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 25),
+                  ),
+                ],
+              ),
             ),
           );
         } else if (state is WeatherFaliureState) {
--- lib/notes.md
+++ lib/notes.md
@@ -2,7 +2,8 @@ Color(0xff5AB2FF)
       search pop to remove 
       Color(0xff62a1c7)
 
-
+Color(0xff62a1c7),Color(0xff)
       #01BFFF
       #7F9BA6
       #4E6969
+      colors: [Color(0xff62a1c7),Color(0xffFFBF78)],
